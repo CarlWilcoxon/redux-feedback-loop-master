83d4a05411e27e99dd46823f0acbd00f0e234c3c
CarlWilcoxon <62306383+CarlWilcoxon@users.noreply.github.com>
2020-07-18 16:48:22 -0500

    updated dependencies and added a Homepage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,8 +3,8 @@ import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import axios from 'axios';
 import './App.css';
 
-// import Home from '../Home/Home.js';
-// import Feels from '../Feels/Feels.js';
+import Home from '../Home/Home.js';
+import Feels from '../Feels/Feels.js';
 // import Understand from '../Understand/Understand.js';
 // import Support from '../Support/Support.js';
 
@@ -22,10 +22,10 @@ class App extends Component {
         </nav>
         <Router>
           <main>
-            {/* <Route exact path="/" component={Home} />
+            <Route exact path="/" component={Home} />
             <Route path="/feels" component={Feels} />
             <Route path="/understand" component={Understand} />
-            <Route path="/support" component={Support} /> */}
+            <Route path="/support" component={Support} />
           </main>
         </Router>
       </div>
---
+++ src/components/Home/Home.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Home extends Component {
+
+ nextPage = () => {
+    // Go to start feedback
+    this.props.history.push('/feels');
+  }
+
+
+  render() {
+    return (
+      <>
+        <button onClick={ this.nextPage }>Submit</button>
+      </>
+    );
+  }
+}
+
+// This function says what to put on Componet props
+const putReduxStateOnProps = (reduxState) => ({
+  reduxState
+});
+
+// connect() will make the connection between the redux store & our Component
+// connect gives us `dispatch`, this.props.dispatch( action )
+// to see redux state, send function to connect
+export default connect(putReduxStateOnProps)(Home);
