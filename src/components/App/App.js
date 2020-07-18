944673071783126cb6c729fa0ec745481ad0ddba
CarlWilcoxon <62306383+CarlWilcoxon@users.noreply.github.com>
2020-07-18 17:04:04 -0500

    Added a couple routes I forgot.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,6 +7,8 @@ import Home from '../Home/Home.js';
 import Feels from '../Feels/Feels.js';
 // import Understand from '../Understand/Understand.js';
 // import Support from '../Support/Support.js';
+// import Comment from '../Comment/Comment.js';
+// import Review from '../Review/Review.js';
 
 class App extends Component {
   render() {
@@ -25,7 +27,9 @@ class App extends Component {
             <Route exact path="/" component={Home} />
             <Route path="/feels" component={Feels} />
             {/* <Route path="/understand" component={Understand} />
-            <Route path="/support" component={Support} /> */}
+            <Route path="/support" component={Support} />
+            <Route path="/comment" component={Comment} />
+            <Route path="/review" component={Review} /> */}
           </main>
         </Router>
       </div>
