ab86d58bcb3f60860158bd5c573286ea2600789f
CarlWilcoxon <62306383+CarlWilcoxon@users.noreply.github.com>
2020-07-18 22:26:13 -0500

    added axios to App, passed it to review via props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
-// import axios from 'axios';
+import { connect } from 'react-redux';
+import axios from 'axios';
 import './App.css';
 
 import Home from '../Home/Home.js';
@@ -10,7 +11,20 @@ import Support from '../Support/Support.js';
 import Comment from '../Comment/Comment.js';
 import Review from '../Review/Review.js';
 
+
 class App extends Component {
+
+  submitToServer = (feedback) =>{
+    axios.post('/feedback', {data: feedback} )
+    .then( (response) => {
+      alert('Feedback sucessfully received!');
+      this.props.history.push('/home');
+    })
+    .catch( (err) => {
+      alert('An error has occured, please try again!');
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -19,17 +33,18 @@ class App extends Component {
             Feedback
           </h1>
           <h2>
-            Don't forget it.
+            <i>Don't forget it.</i>
           </h2>
         </nav>
         <Router>
           <main>
             <Route exact path="/" component={Home} />
+            <Route path="/home" component={Home} />
             <Route path="/feels" component={Feels} />
             <Route path="/understand" component={Understand} />
             <Route path="/support" component={Support} />
             <Route path="/comment" component={Comment} />
-            <Route path="/review" component={Review} />
+            <Route path="/review" submitToServer={this.submitToServer} component={Review} />
           </main>
         </Router>
       </div>
@@ -37,4 +52,12 @@ class App extends Component {
   }
 }
 
-export default App;
+// This function says what to put on Componet props
+const putReduxStateOnProps = (reduxState) => ({
+  reduxState
+});
+
+// connect() will make the connection between the redux store & our Component
+// connect gives us `dispatch`, this.props.dispatch( action )
+// to see redux state, send function to connect
+export default connect(putReduxStateOnProps)(App);
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -3,27 +3,12 @@ import { connect } from 'react-redux';
 
 class Review extends Component {
 
-  // Setup the button handler
-  submitFeedbackToServer = (event) => {
-    event.preventDefault();
-    // send the value to the reducer
-    this.props.dispatch( { type: 'SUBMIT_TO_SERVER', payload: this.props.reduxState.setFeedback } )
-
-    // Go to next page
-    this.props.history.push('/home');
-  }
-
-  confirmationDialog = (event) => {
-    // Go to next page
-    this.props.history.push('/home');
-  }
-
   render() {
     return (
       <>
       <p>{JSON.stringify(this.props.reduxState.setFeedback)}</p>
 
-      <button onClick={ this.submitFeedback }>Submit</button>
+      <button onClick={ () => this.props.submitToServer(this.props.reduxState.setFeedback) }>Submit</button>
 
       </>
     );
